66664ae0ab41c670f3ceb4c3f53a19d1f495073e
devGiovanni95 <giovanni.DevJava@hotmail.com>
2021-02-08 21:52:18 -0300

    fundamentos

--- src/fudamentos/TipoString.java
+++ src/fudamentos/TipoString.java
@@ -18,6 +18,37 @@ public class TipoString {
         System.out.println(s.equals("boa tarde"));
         System.out.println(s.equalsIgnoreCase("boa tarde"));
 
+        var nome = "Pedro";
+        var sobrenome = "Santos";
+        var idade = 33;
+        var salario = 12345.987;
+
+        String maisUmaFrase = "Nome: " + nome + "\nSobrenome: " + sobrenome +
+                "\nIdade: " + idade + "\nSalario: " + salario + "\n\n";
+
+        System.out.println(maisUmaFrase);
+
+        System.out.println("Nome: " + nome + "\nSobrenome: " + sobrenome +
+                "\nIdade: " + idade + "\nSalario: " + salario + "\n\n");
+
+        //%s string
+        //%d inteiros
+        //%f float  %.2f formatado para 2 casas depois do ponto
+        //%n pula uma linha
+
+        System.out.printf("O senhor %s %s tem %d anos  e ganha R$ %.2f.",
+                nome, sobrenome, idade, salario);
+
+        String frase = String.format("\nO senhor %s %s tem %d anos  e ganha R$ %.2f.",
+                nome, sobrenome, idade, salario  );
+
+        System.out.println(frase);
+
+
+        System.out.println("Frase qualquer".contains("qual"));   // dentro da frase comtem a palavra .....
+        System.out.println("Frase qualquer".indexOf("qual"));  //o indice de onde comeca qual
+        System.out.println("Frase qualquer".substring(6)); // começa a partir do indice fornecido
+        System.out.println("Frase qualquer".substring(6,8));  // comeca do indice mostrado e termina no outro indice fornecido
 
     }
 }
