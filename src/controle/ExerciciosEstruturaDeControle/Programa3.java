71226005bbf3d40cfe3c51dc8146e909797e6b8e
devGiovanni95 <giovanni.DevJava@hotmail.com>
2021-02-24 21:17:07 -0300

    Finalizando modulo de controle

--- src/controle/ExerciciosEstruturaDeControle/Programa3.java
+++ src/controle/ExerciciosEstruturaDeControle/Programa3.java
@@ -1,4 +1,30 @@
 package controle.ExerciciosEstruturaDeControle;
 
+import java.util.Scanner;
+
 public class Programa3 {
+    public static void main(String[] args) {
+        Scanner dados = new Scanner(System.in);
+
+        System.out.println("Digite a primeira nota: ");
+        double nota1 = dados.nextDouble();
+
+        System.out.println("Digite a segunda nota: ");
+        double nota2 = dados.nextDouble();
+
+        double media = (nota1 + nota2) / 2;
+
+
+
+        if (media >= 7 && media <= 10) {
+            System.out.println("O aluno teve a média de " + media + " e foi Aprovado");
+        } else if (media < 7 && media >= 4) {
+            System.out.println("O aluno teve a média de " + media + " e esta de Recuperação");
+        } else if (media < 4 && media >= 0) {
+            System.out.println("O aluno teve a média de " + media + " e foi Reprovado");
+        }else {
+            System.out.println("Por favor reveja os dados e tente novamente");
+        }
+
+    }
 }
--- src/controle/ExerciciosEstruturaDeControle/Programa7.java
+++ src/controle/ExerciciosEstruturaDeControle/Programa7.java
@@ -1,4 +1,18 @@
 package controle.ExerciciosEstruturaDeControle;
 
+import java.util.Scanner;
+
 public class Programa7 {
+    public static void main(String[] args) {
+        Scanner dados = new Scanner(System.in);
+
+        System.out.println(" Digite um numero: ");
+        int numero = dados.nextInt();
+
+        while (numero >= 0 ){
+            int soma = 0 + numero;
+
+            System.out.println(soma);
+        }
+    }
 }
