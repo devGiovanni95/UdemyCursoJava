bae128dc3008c78192c5c8bc2ff423f79eadd017
devGiovanni95 <giovanni.DevJava@hotmail.com>
2021-04-22 19:49:56 -0300

    Lambdas
    Foreach Funcional

--- src/lambdas/Calculo.java
+++ src/lambdas/Calculo.java
@@ -1,7 +1,21 @@
 package lambdas;
 
+@FunctionalInterface
 public interface Calculo {
 
     //    public  abstract double executar(double a, double b);
     double executar(double a, double b);
+
+
+//Quando utilizamos o metodo default nao geramos erro na FunctionaInterface
+//mas se utilizarmos outro metodo ja teriamos problemas
+
+    default String legal(){
+        return "Legal";
+    }//posso acessar os metodos aqui dentro
+
+    //uma outra possibilidade e incrementar um metodo estatico
+    static String muitoLegal(){
+        return "Muito legal";
+    }//ja aqui nao da pra acessar os metodos
 }
