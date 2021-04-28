27cbdf952d232fed053bff23d40e54e6b5a50d9b
devGiovanni95 <giovanni.DevJava@hotmail.com>
2021-04-27 22:17:19 -0300

    Stream API

---
+++ src/streams/Map.java
@@ -0,0 +1,11 @@
+package streams;
+
+import java.util.function.Consumer;
+
+public class Map {
+
+    public static void main(String[] args) {
+
+        Consumer<String> print = System.out::println;
+    }
+}
